6e8b56ba5e640dbcdc413370d0f1fc27802e7540
David S <dshvimer@pm.me>
2021-03-02 22:31:33 -0500

    play

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+interface RequestConfig {
+  data?: any,
+  method: 'GET' | ' POST'
+};
+
+const config = ({data, method}: RequestConfig) => {
+  const conf = {
+    method: method, // *GET, POST, PUT, DELETE, etc.
+  }
+
+  if (method === 'GET') return conf
+  else return { ...conf, body: JSON.stringify(data)}
+}
+
+const status200 = () => {
+  return {
+    url: 'http://httpstat.us/200',
+    config: config({method: 'GET'})
+  }
+}
+
+const status401 = () => {
+  return {
+    url: 'http://httpstat.us/401',
+    config: config({method: 'GET'})
+  }
+}
+
+interface Request {
+  url: string,
+  config: RequestConfig
+}
+
+const request = async ({url, config}: Request) => {
+    try {
+      const res = await fetch(url, config)
+      if (res.status === 401) {
+        const tokenRes = await fetch('http://httpstat.us/401')
+        if (tokenRes.status === 200) {
+          // SET THE TOKEN HERE
+          const retry = await fetch(url, config)
+          return Promise.resolve(retry)
+        }
+        else {
+          return Promise.reject(tokenRes)
+        }
+      }
+      else {
+        return Promise.resolve(res)
+      }
+    }
+    catch(e) {
+      return Promise.reject(e)
+    }
+}
+
 
 function App() {
+  const [text, setText] = useState("")
+
+  useEffect(() => {
+    request(status401())
+    .then(res => {
+      console.log('res', res)
+      return res.text()
+    })
+    .then(text => setText(text))
+    .catch(error => console.log('error', error))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {text}
     </div>
   );
 }
